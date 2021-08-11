72531971a350e139711fbbaddabd74f817bd9d71
diego-vini <analistamm@gmail.com>
2021-08-11 07:50:13 -0300

    Desafios 1 ao 7 resolvidos

--- 05-project_playground_functions/sd-09-project-playground-functions/src/challenges.js
+++ 05-project_playground_functions/sd-09-project-playground-functions/src/challenges.js
@@ -1,36 +1,66 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(boolean1, boolean2) {
+  if(boolean1 && boolean2) {
+    return true
+  } else {
+    return false
+  }
 }
-
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height) / 2
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(phrase) {
+  return phrase.split(' ')
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(arrayStr) {
+  return arrayStr[arrayStr.length - 1] + ', ' +  arrayStr[0] + ' .'
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  const pointsForWins = 3;
+  const pointsForTies = 1;
+  let totPoints = (pointsForWins * wins) + (pointsForTies * ties);
+  return totPoints;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayNumbers) {
+  let countNumbers = 0
+  let repeat = 0
+  let higherNumber = 0
+  for(let number in arrayNumbers) {
+    let x = arrayNumbers[number]
+    for(let toCompare in arrayNumbers){
+      if(x === arrayNumbers[toCompare]){
+        countNumbers += 1
+      }
+    }
+    if(x > higherNumber){
+      higherNumber = x
+      repeat = countNumbers
+    }
+    countNumbers = 0
+  }
+  return repeat
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(cat1, cat2, mouse) {
+  let distCat1ToMouse = mouse - cat1;
+  let distCat2ToMouse = mouse - cat2;
+  if(distCat1ToMouse === distCat2ToMouse){
+    return 'os gatos trombam e o rato foge'
+  } else if (distCat1ToMouse < distCat2ToMouse){
+    return 'cat1'
+  } else {
+    return 'cat2'
+  }
 }
 
 // Desafio 8
